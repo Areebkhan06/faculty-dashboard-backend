69d558aeae70a8d955d92a8e04166a79657467dc
Areebkhan06 <areebkhan4575@gmail.com>
2026-03-10 21:40:01 +0530

    froned url

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import FacultyRouter from "./router/facultyRouter.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" ,credentials:true}));
+app.use(cors({ origin: process.env.FRONTEND_URL ,credentials:true}));
 
 app.use(express.json());
 
